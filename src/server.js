863007dd3fc2ea4d49c160da007eba9a8b22ce02
Delfi Sehidic <delfi.sehidic@gmail.com>
2021-02-16 12:21:23 +0100

    add directories

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log('Hello')
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+dotenv.config()
+
+const app = express()
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+})
